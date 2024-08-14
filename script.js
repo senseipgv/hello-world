a528acdccb8f8ebcc65d1838bb44ec4c74d18727
Profa. Patricia <146781553+senseipgv@users.noreply.github.com>
2024-08-14 18:07:36 -0300

    Hello Matemático

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const nome = prompt('Como tu se chama?');
+
+var mensagem = document.getElementById('message');
+mensagem.textContent = `Bem-vindo, ${nome}! Vamos aprender matemática juntos.`;
+
+
